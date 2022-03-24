31dddfef959bf2cf2720e4fce86bd413819c6f79
1318PK <ainur.hak2004@gmail.com>
2022-03-24 09:30:53 +0300

    Добавьте файлы проекта.

---
+++ AuorizRegister/Data/Register.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Rendering;
+using Microsoft.AspNetCore.Components.Web;
+
+namespace AuorizRegister.Data
+{
+    public class Register : Authoriz
+    {
+
+    }
+}
